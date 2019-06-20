83c3aca442cfccd6a73bcb312077e247fed0d006
Tommaso Barbato <epistrephein@gmail.com>
2019-06-20 07:32:35 +0200

    Log errors to stderr.log file

--- config/schedule.example.rb
+++ config/schedule.example.rb
@@ -5,7 +5,7 @@ job_type :ruby, 'cd :path && bundle exec ruby :task :output'
 env :PATH, ENV['PATH']
 
 set :chronic_options, hours24: true
-set :output, error: 'log/errors.log'
+set :output, error: 'log/stderr.log'
 
 every 1.day, at: '10:30' do
   ruby 'rubyproject.rb'
