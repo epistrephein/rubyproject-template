9b5e537853816d3ad11f4ea634284afb58c1ac41
Tommaso Barbato <epistrephein@gmail.com>
2020-03-06 06:58:22 +0100

    Add ordered require_relative to main file

--- rubyproject.rb
+++ rubyproject.rb
@@ -1,3 +1,8 @@
 # frozen_string_literal: true
 
 require 'bundler/setup'
+
+require_relative 'lib/config'
+require_relative 'lib/logger'
+require_relative 'lib/database'
+require_relative 'lib/telegram'
