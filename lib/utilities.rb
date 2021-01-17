68a6ec7fe3da6f437f5a3b9ac1a0d4534fcf4f99
Tommaso Barbato <tommasobarbato@gmail.com>
2021-01-17 09:02:43 +0100

    Use simpler calculation for linear delay

--- lib/utilities.rb
+++ lib/utilities.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-# Retry block with custom delay
+# Retry block with custom linear delay
 def with_retries(retries: 3, rescue_ex: [StandardError], swallow_ex: false, delay: 0)
   yield
 rescue *rescue_ex => e
   attempts ||= retries
-  delay_for  = attempts * delay * -1 + (retries * delay + delay)
+  delay_for  = (retries - attempts + 1) * delay
 
   if (attempts -= 1).positive?
     sleep delay_for
