e196d5f2e94422352f84ed93eb0372dcdebcdaca
Tommaso Barbato <tommasobarbato@gmail.com>
2021-08-18 12:35:53 +0200

    Don't eager_load models
    This way we can avoid Zeitwerk trying to initialize models
    before their Sequel table has been created in a migration.

--- lib/loader.rb
+++ lib/loader.rb
@@ -4,10 +4,15 @@ require 'zeitwerk'
 
 # Autoload code.
 loader = Zeitwerk::Loader.new
+
 loader.push_dir(__dir__)
+
 loader.collapse("#{__dir__}/concerns")
 loader.collapse("#{__dir__}/models")
 loader.collapse("#{__dir__}/utilities")
+
 loader.do_not_eager_load(__FILE__)
+loader.do_not_eager_load("#{__dir__}/models")
+
 loader.setup
 loader.eager_load
