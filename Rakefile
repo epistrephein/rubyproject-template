76e004b79e76c58f629b474ae9f58e2d88229ffc
Tommaso Barbato <tommasobarbato@gmail.com>
2021-08-19 09:47:44 +0200

    Extract loader into its own class

--- Rakefile
+++ Rakefile
@@ -7,6 +7,7 @@ ROOT_DIR = Pathname(__dir__).expand_path
 $LOAD_PATH.unshift(ROOT_DIR) unless $LOAD_PATH.include?(ROOT_DIR)
 
 require 'lib/loader'
+Loader.load!
 
 Rake.add_rakelib 'tasks/**'
 
--- lib/loader.rb
+++ lib/loader.rb
@@ -2,23 +2,58 @@
 
 require 'zeitwerk'
 
-# Autoload code.
-loader = Zeitwerk::Loader.new
+class Loader
+  PUSH               = [__dir__].freeze
+  EAGER_LOAD         = true
+  EXCLUDE_EAGER_LOAD = %w[models].freeze
+  COLLAPSE           = %w[concerns models utilities].freeze
+  TRY_REQUIRE        = %w[dotenv/load].freeze
 
-loader.push_dir(__dir__)
+  class << self
+    def load!(**opts)
+      @loader = Zeitwerk::Loader.new
+      @opts   = opts
 
-loader.collapse("#{__dir__}/concerns")
-loader.collapse("#{__dir__}/models")
-loader.collapse("#{__dir__}/utilities")
+      push!
+      collapse!
+      @loader.setup
 
-loader.do_not_eager_load(__FILE__)
-loader.do_not_eager_load("#{__dir__}/models")
+      try_require!
 
-begin
-  require 'dotenv/load'
-rescue LoadError
-  nil
+      exclude_eager_load!
+      @loader.eager_load if eager_load?
+    end
+
+    private
+
+    def push!
+      paths = @opts[:push] || PUSH
+      paths.each { |path| @loader.push_dir(path) }
+    end
+
+    def collapse!
+      paths = @opts[:collapse] || COLLAPSE
+      paths.each { |path| @loader.collapse("#{__dir__}/#{path}") }
+    end
+
+    def try_require!
+      gems = @opts[:try_require] || TRY_REQUIRE
+      gems.each do |gem|
+        require gem
+      rescue LoadError
+        next
+      end
+    end
+
+    def exclude_eager_load!
+      paths = @opts[:exclude_eager_load] || EXCLUDE_EAGER_LOAD
+      paths.each { |path| @loader.do_not_eager_load("#{__dir__}/#{path}") }
+    end
+
+    def eager_load?
+      return EAGER_LOAD if @opts[:eager_load].nil?
+
+      @opts[:eager_load]
+    end
+  end
 end
-
-loader.setup
-loader.eager_load
