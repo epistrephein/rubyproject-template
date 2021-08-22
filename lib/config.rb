854cc29adee1a364798c1515222027c2541aff02
Tommaso Barbato <tommasobarbato@gmail.com>
2021-08-22 20:10:12 +0200

    Extract Config REQUIRED_KEYS in a constant

--- lib/config.rb
+++ lib/config.rb
@@ -3,7 +3,8 @@
 require 'yaml'
 
 class Config
-  CONFIG_FILE = File.join(__dir__, '..', 'config', 'config.yml')
+  CONFIG_FILE   = ENV['CONFIG_FILE'] || File.join(__dir__, '..', 'config', 'config.yml')
+  REQUIRED_KEYS = %w[mysql telegram aws:s3 aws:ses].freeze
 
   class << self
     def [](*args)
@@ -21,10 +22,8 @@ class Config
     end
 
     def load_and_validate!
-      config = YAML.load_file(ENV['CONFIG_FILE'] || CONFIG_FILE)
-
-      required_keys = %w[mysql telegram aws:s3 aws:ses]
-      missing_keys  = required_keys.reject { |k| config.dig(*k.split(':')) }
+      config       = YAML.load_file(CONFIG_FILE)
+      missing_keys = REQUIRED_KEYS.reject { |k| config.dig(*k.split(':')) }
 
       unless missing_keys.empty?
         raise "Invalid config file, missing keys: #{missing_keys.join(', ')}"
