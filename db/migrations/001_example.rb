282b789b45963a7f17e30ce53a90afb43b2bdd48
Tommaso Barbato <tommasobarbato@gmail.com>
2021-08-24 16:18:14 +0200

    Add created_at and updated_at to example

--- db/migrations/001_example.rb
+++ db/migrations/001_example.rb
@@ -4,7 +4,9 @@ Sequel.migration do
   up do
     create_table(:examples) do
       primary_key :id
-      String :name, null: false
+      String      :name, null: false
+      DateTime    :created_at
+      DateTime    :updated_at
     end
   end
 
